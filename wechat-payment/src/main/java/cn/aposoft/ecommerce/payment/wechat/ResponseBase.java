4e330736f0d3d8b641461a10da2e6cf502a27134
yujinshui <sc_yujinshui@126.com>
2015-10-26 23:18:57 +0800

    ResponseBase重命名

--- wechat-payment/src/main/java/cn/aposoft/ecommerce/payment/wechat/ResponseBase.java
+++ wechat-payment/src/main/java/cn/aposoft/ecommerce/payment/wechat/ResponseBase.java
@@ -1,12 +1,12 @@
 package cn.aposoft.ecommerce.payment.wechat;
 
 /**
- * 支付，退款返回部分通用字段
+ * 支付，退款返回部分通用字段 查询xml样式 查询参数xml格式：
  * 
  * @author Yujinshui
  *
  */
-public class PayResponseBase {
+public class ResponseBase {
 
 	/**
 	 * <font color=red>必需</font>-返回状态码
@@ -42,7 +42,13 @@ public class PayResponseBase {
 	 */
 	private String result_code;
 	/**
-	 * 错误代码
+	 * 错误代码:
+	 * <ul>
+	 * <li>名称 描述 原因 解决方案</li>
+	 * <li>ORDERNOTEXIST 此交易订单号不存在 查询系统中不存在此交易订单号
+	 * 该API只能查提交支付交易返回成功的订单，请商户检查需要查询的订单号是否正确</li>
+	 * <li>SYSTEMERROR 系统错误 后台系统返回错误 系统异常，请再调用发起查询</li>
+	 * </ul>
 	 */
 	private String err_code;
 	/**
