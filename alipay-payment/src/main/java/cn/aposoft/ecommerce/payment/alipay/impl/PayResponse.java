ae9326f177f0fd43f03d2aa42687c2be1a802a81
yujinshui <sc_yujinshui@126.com>
2015-11-19 12:52:13 +0800

    Response通用参数的提取

--- alipay-payment/src/main/java/cn/aposoft/ecommerce/payment/alipay/impl/PayResponse.java
+++ alipay-payment/src/main/java/cn/aposoft/ecommerce/payment/alipay/impl/PayResponse.java
@@ -1,5 +1,7 @@
 package cn.aposoft.ecommerce.payment.alipay.impl;
 
+import cn.aposoft.ecommerce.payment.alipay.ResponseBase;
+
 /**
  * 
  * 响应结果封装bean
@@ -19,7 +21,7 @@ package cn.aposoft.ecommerce.payment.alipay.impl;
  * 
  * @author Yujinshui
  */
-public class PayResponse {
+public class PayResponse extends ResponseBase {
 
 	/*************** 当返回内容无法正常解析时，直接进行原内容返回 ******************/
 	/**
@@ -50,77 +52,6 @@ public class PayResponse {
 	}
 
 	/**
-	 * 通过比对支付宝返回的sign签名，判断是否为true
-	 * <p>
-	 * true:是支付宝返回结果<br>
-	 * false:警告！非支付宝返回结果
-	 */
-	private boolean isAliPay;
-
-	/**
-	 * 通过比对支付宝返回的sign签名，判断是否为true
-	 * <p>
-	 * true:是支付宝返回结果<br>
-	 * false:警告！非支付宝返回结果
-	 * 
-	 * @return
-	 * @author Yujinshui
-	 * @time 2015年11月18日 下午3:10:36
-	 */
-	public boolean getIsAliPay() {
-		return isAliPay;
-	}
-
-	/**
-	 * 通过比对支付宝返回的sign签名，判断是否为true
-	 * <p>
-	 * true:是支付宝返回结果<br>
-	 * false:警告！非支付宝返回结果
-	 * 
-	 * @param isAliPay
-	 * @author Yujinshui
-	 * @time 2015年11月18日 下午3:10:41
-	 */
-	public void setIsAliPay(boolean isAliPay) {
-		this.isAliPay = isAliPay;
-	}
-
-	/**
-	 * 请求是否成功[T:成功。F:失败]
-	 */
-	private String is_success;
-	/**
-	 * 签名类型
-	 */
-	private String sign_type;
-	/************* 通用参数 ***************/
-	/**
-	 * 返回支付宝sign签名
-	 */
-	private String sign;
-
-	/**
-	 * 错误代码
-	 * <p>
-	 * 请求成功时，不存在本参 数；<br>
-	 * 请求失败时，本参数为错误 代码，参见“ 10.2 接入错 误码”和“ 10.3 系统错误 码”。
-	 */
-	private String error;
-	/**
-	 * 响应码[SUCCESS/FAIL]
-	 */
-	private String result_code;
-	/**
-	 * 支付宝交易号
-	 * <p>
-	 * 该交易在支付宝系统中的交易 流水号。 最短 16 位，最长 64 位。
-	 */
-	private String trade_no;
-	/**
-	 * 商户网站 唯一订单 号
-	 */
-	private String out_trade_no;
-	/**
 	 * 凭证类型
 	 * <p>
 	 * 凭证类型，目前仅支持 （二维码）。 qrcode
@@ -139,168 +70,10 @@ public class PayResponse {
 	 * 二维码浏览器地址【图片尺寸：小】
 	 */
 	private String small_pic_url;
-	/************* 失败返回参数 ***************/
-	/**
-	 * 详细错误码
-	 * <p>
-	 * 对返回响应码进行原因说明， 请参见“ 10.1 业务错误码”。 当 result_code 响应码为 SUCCESS 时，不返回该参数。
-	 */
-	private String detail_error_code;
-	/**
-	 * 错误码说明
-	 */
-	private String detail_error_des;
-
 	/**
 	 * 二维码浏览器地址【图片尺寸：大】
 	 */
 	private String big_pic_url;
-
-	/**
-	 * 返回支付宝sign签名
-	 */
-	public String getSign() {
-		return sign;
-	}
-
-	/**
-	 * 返回支付宝sign签名
-	 */
-	public void setSign(String sign) {
-		this.sign = sign;
-	}
-
-	/**
-	 * 签名类型
-	 * 
-	 * @return
-	 * @author Yujinshui
-	 * @time 2015年11月13日 下午4:38:09
-	 */
-	public String getSign_type() {
-		return sign_type;
-	}
-
-	/**
-	 * 签名类型
-	 * 
-	 * @param sign_type
-	 * @author Yujinshui
-	 * @time 2015年11月13日 下午4:38:24
-	 */
-	public void setSign_type(String sign_type) {
-		this.sign_type = sign_type;
-	}
-
-	/**
-	 * 返回标志码[SUCCESS/FAIL]
-	 * 
-	 * @return
-	 * @author Yujinshui
-	 * @time 2015年11月13日 下午4:38:34
-	 */
-	public String getResult_code() {
-		return result_code;
-	}
-
-	/**
-	 * 返回标志码[SUCCESS/FAIL]
-	 * 
-	 * @param result_code
-	 * @author Yujinshui
-	 * @time 2015年11月13日 下午4:38:39
-	 */
-	public void setResult_code(String result_code) {
-		this.result_code = result_code;
-	}
-
-	/**
-	 * 商户交易订单号
-	 * 
-	 * @return
-	 * @author Yujinshui
-	 * @time 2015年11月13日 下午4:38:45
-	 */
-	public String getOut_trade_no() {
-		return out_trade_no;
-	}
-
-	/**
-	 * 商户交易订单号
-	 * 
-	 * @return
-	 * @author Yujinshui
-	 * @time 2015年11月13日 下午4:38:45
-	 */
-	public void setOut_trade_no(String out_trade_no) {
-		this.out_trade_no = out_trade_no;
-	}
-
-	/**
-	 * 请求是否成功[T:成功。F:失败]
-	 * 
-	 * @return
-	 * @author Yujinshui
-	 * @time 2015年11月13日 下午4:38:45
-	 */
-	public String getIs_success() {
-		return is_success;
-	}
-
-	/**
-	 * 请求是否成功[T:成功。F:失败]
-	 * 
-	 * @return
-	 * @author Yujinshui
-	 * @time 2015年11月13日 下午4:38:45
-	 */
-	public void setIs_success(String is_success) {
-		this.is_success = is_success;
-	}
-
-	/**
-	 * 错误码
-	 * 
-	 * @return
-	 * @author Yujinshui
-	 * @time 2015年11月13日 下午4:38:45
-	 */
-	public String getDetail_error_code() {
-		return detail_error_code;
-	}
-
-	/**
-	 * 错误码
-	 * 
-	 * @return
-	 * @author Yujinshui
-	 * @time 2015年11月13日 下午4:38:45
-	 */
-	public void setDetail_error_code(String detail_error_code) {
-		this.detail_error_code = detail_error_code;
-	}
-
-	/**
-	 * 错误码说明
-	 * 
-	 * @return
-	 * @author Yujinshui
-	 * @time 2015年11月13日 下午4:38:45
-	 */
-	public String getDetail_error_des() {
-		return detail_error_des;
-	}
-
-	/**
-	 * 错误码说明
-	 * 
-	 * @return
-	 * @author Yujinshui
-	 * @time 2015年11月13日 下午4:38:45
-	 */
-	public void setDetail_error_des(String detail_error_des) {
-		this.detail_error_des = detail_error_des;
-	}
 
 	/**
 	 * 二维码浏览器地址【图片尺寸：小】
@@ -410,22 +183,6 @@ public class PayResponse {
 	 */
 	public void setBig_pic_url(String big_pic_url) {
 		this.big_pic_url = big_pic_url;
-	}
-
-	public String getError() {
-		return error;
-	}
-
-	public void setError(String error) {
-		this.error = error;
-	}
-
-	public String getTrade_no() {
-		return trade_no;
-	}
-
-	public void setTrade_no(String trade_no) {
-		this.trade_no = trade_no;
 	}
 
 }
